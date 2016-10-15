1839b656c215b8d8d604c87caa15970c8817b1c6
Tim Bennett <bennetimo@gmail.com>
2016-10-15 16:16:15 +0100

    integrate firebase ads

--- app/src/main/java/io/coderunner/chordmaster/WelcomeActivity.java
+++ app/src/main/java/io/coderunner/chordmaster/WelcomeActivity.java
@@ -3,11 +3,24 @@ package io.coderunner.chordmaster;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        MobileAds.initialize(getApplicationContext(), getString(R.string.firebase_app_id));
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("3D7A81A8C6EFE58743D4EA139908457B")
+                .build();
+        mAdView.loadAd(adRequest);
+
     }
 }
