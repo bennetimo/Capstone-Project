4aa8ab14449ffdbe02035a43ff9436f130dacf4d
Tim Bennett <bennetimo@gmail.com>
2016-10-16 17:47:45 +0100

    show basic dialogue with score picker when time is up

--- app/src/main/java/io/coderunner/chordmaster/PracticeFragment.java
+++ app/src/main/java/io/coderunner/chordmaster/PracticeFragment.java
@@ -1,17 +1,19 @@
 package io.coderunner.chordmaster;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import butterknife.BindInt;
 import butterknife.BindView;
@@ -26,6 +28,9 @@ public class PracticeFragment extends Fragment {
     @BindInt(R.integer.countdown_ms) int mCountdownMs;
     @BindInt(R.integer.leadin_ms) int mLeadinMs;
     @BindInt(R.integer.countdown_interval_ms) int mCountdownIntervalMs;
+    @BindInt(R.integer.score_picker_max_value) int mScorePickerMax;
+    @BindInt(R.integer.score_picker_min_value) int mScorePickerMin;
+    @BindInt(R.integer.score_picker_default_value) int mScorePickerDefault;
     private Context mContext;
 
     @Override
@@ -59,7 +64,37 @@ public class PracticeFragment extends Fragment {
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(mContext, getString(R.string.timer_expired), Toast.LENGTH_LONG).show();
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                        LayoutInflater inflater = getActivity().getLayoutInflater();
+                        View dialogue = inflater.inflate(R.layout.dialogue_times_up, null);
+
+
+                        NumberPicker scorePicker = (NumberPicker) dialogue.findViewById(R.id.scorePicker);
+                        scorePicker.setMaxValue(mScorePickerMax);
+                        scorePicker.setMinValue(mScorePickerMin);
+                        scorePicker.setValue(mScorePickerDefault);
+
+                        builder.setView(dialogue);
+
+                        builder.setMessage(R.string.dialogue_times_up_message)
+                                .setTitle(R.string.dialogue_times_up_title);
+
+
+                        // Add the buttons
+                        builder.setPositiveButton(R.string.dialogue_times_up_ok_button, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User clicked OK button
+                            }
+                        });
+                        builder.setNegativeButton(R.string.dialogue_times_up_cancel_button, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                            }
+                        });
+
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
                     }
                 });
             }
